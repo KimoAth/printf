b9225f3616ea41db3445afa72e7b0d131c34f1cb
kimoAth <kimo.daniel11@gmail.com>
2023-04-23 23:45:04 +0000

    commit changes

--- _printf.c
+++ _printf.c
@@ -7,8 +7,7 @@
  */
 int _printf(const char *format, ...)
 {
-	char *per1 = "csiduxXop%";
-	int i = 0;
+
 	va_list args;
 	int count = 0;
 
@@ -24,18 +23,10 @@ int _printf(const char *format, ...)
 			{
 				for (; *format != '%'; format++)
 				{
-					if (*format == '\0')
-						return (-1);
 				}
 			}
-			for (; per1[i] != '\0'; i++)
-			{
-				if (per1[i] == *format)
-				{
-					count = print_args1(args, *format, count);
-				}
-			}
-			i = 0;
+
+			count = print_args1(args, *format, count);
 		}
 		else
 		{
