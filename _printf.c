2ec0cad65db859322c7d1aaa7e9fd4a62f282c5b
kimoAth <kimo.daniel11@gmail.com>
2023-04-23 14:01:36 +0000

    karima edited string

--- _printf.c
+++ _printf.c
@@ -11,7 +11,7 @@ int _printf(const char *format, ...)
 	va_list args;
 	int count = 0;
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 	va_start(args, format);
 	for (; *format; format++)
